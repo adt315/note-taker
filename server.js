60c2d92db1ff48ec0f982ce09ad1d13dc94fb12b
adt315 <angela_dyan@hotmail.com>
2020-12-20 20:48:29 -0500

    add delete

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
-
-const express = require ("express");
-const uuid = require ("uuid");
-const fs = require ("fs");
-const path = require ("path");
-const dbjson = require ("./db/db.json");
-
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.urlencoded({ extended:false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 //Notes API Routes
 app.use("/api/notes", require("./routes/api/notes"));
 
-// HTML Routes 
+// HTML ROUTES
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/notes", (req,res) => {
-    res.sendFile(__dirname + "/public/notes.html");
-});   
-
-// The application should have a `db.json` file on the backend that will be used to store 
-// and retrieve notes using the `fs` module.
-
+app.get("/notes", (req, res) => {
+  res.sendFile(__dirname + "/public/notes.html");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
